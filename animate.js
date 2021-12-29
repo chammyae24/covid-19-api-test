94d43b4cf9a1028dc410fcaf35cb26bb5f84a906
Cham Myae <88666000+chammyae24@users.noreply.github.com>
2021-12-30 05:57:44 +0630

    Edit animation

--- animate.js
+++ animate.js
@@ -2,12 +2,7 @@ function animationStart() {
   const animateStarts = document.querySelectorAll(".animation");
 
   animateStarts.forEach(anime => {
-    anime.style.transform = "scale(0)";
-    anime.style.transition = "transform 0.35s cubic-bezier(1,0,0,1) 0s";
-
-    setTimeout(() => {
-      anime.style.transform = "scale(1)";
-    });
+    anime.style.transform = "scale(1)";
   });
 }
 
