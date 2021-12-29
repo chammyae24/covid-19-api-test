6e9cacb4207ad8f6c2abc96e1d61944cf2d1a243
Cham Myae <88666000+chammyae24@users.noreply.github.com>
2021-12-30 06:06:52 +0630

    Update app.js

--- app.js
+++ app.js
@@ -55,10 +55,10 @@ function addData(country) {
 
   if (cardContainer.childElementCount > 0) {
     cardContainer.insertBefore(clone, cardContainer.firstChild);
-    setTimeout(()=> animationStart(), 300);
+    setTimeout(()=> animationStart, 300);
   } else {
     cardContainer.appendChild(clone);
-    setTimeout(()=> animationStart(), 300);
+    setTimeout(()=> animationStart, 300);
   }
 
   cardImg.src = `https://flagcdn.com/${country.CountryCode.toLowerCase()}.svg`;
@@ -75,7 +75,7 @@ function addData(country) {
     btn.addEventListener("click", e => {
       const targetCard = e.target.parentElement;
       
-      targetCard.classList.add("animation-end");
+      //targetCard.classList.add("animation-end");
       animationEnd(e);
       setTimeout(() => targetCard.remove(), 400);
     })
